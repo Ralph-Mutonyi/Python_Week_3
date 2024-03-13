d6235e35504166073202880d25de2331e4361e79
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-03-13 10:33:33 +0300

    merged remote changes

--- Practices/conditionals/conditionals.py
+++ Practices/conditionals/conditionals.py
@@ -17,27 +17,13 @@ else:
 print("Final bill: " + str(bill))
 
 
-#if else statement
-    
-#if condition:
-#   block of code if condition is true
-
-#else:
-#   block of code if condition is false
 
 
-# if elif else statement
-
-# if condition1:
-# code block1
 
 
-#elif condition2:
-#   code block 2
 
-#else:
-#   code block 3
-    
+
+
 
 
 
---
+++ Practices/conditionals/conditionals1.py
@@ -0,0 +1,6 @@
+#if else statement
+    
+#if condition:
+#   block of code if condition is true
+
+#else:
